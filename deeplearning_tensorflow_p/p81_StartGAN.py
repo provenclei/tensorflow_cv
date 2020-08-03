f6a2078a765017b6349b371a901d2ac9245bb60d
MaoLongTL <18565663062@163.com>
2020-08-03 10:09:08 +0800

    commit StarGAN

--- deeplearning_tensorflow_p/p81_StartGAN.py
+++ deeplearning_tensorflow_p/p81_StartGAN.py
@@ -83,7 +83,7 @@ class MySubTensors:
         lx = tf.placeholder(tf.int64, [None], 'lx')
         self.x = x
         self.l = l
-        self.inputs = [x, lx, v]
+        self.inputs = [x, lx, l]
         l = tf.one_hot(l, 10, dtype=tf.float64)  # 不可训练，无需命名 [-1, 10]
         lx = tf.one_hot(lx, 10, dtype=tf.float64)  # [-1, 10]
 
@@ -103,7 +103,7 @@ class MySubTensors:
         loss1 = -tf.reduce_mean(tf.log(x_v))
         loss2 = -tf.reduce_mean(tf.log(1 - x2_v))
         loss3 = -tf.reduce_mean(tf.log(x2_v))
-        loss4 = -tf.reduce_mean(tf.square(x, x3))
+        loss4 = -tf.reduce_mean(tf.square(x-x3))
         self.losses = [loss1, loss2, loss3, loss4]
 
     def disc(self, x, l):
@@ -112,15 +112,19 @@ class MySubTensors:
         :param x: [-1, 28, 28, 1]
         :return:
         '''
-        filters = self.cfg.base_filters   # 64
+        x = self.encode(x, l, 1)
+        return tf.nn.sigmoid(x)
+
+    def encode(self, x, l, logits: int):
+        filters = self.cfg.base_filters  # 64
         size = (x.shape[1].value, x.shape[2].value)
         # x: [-1, 28, 28, 64]
         x = tf.layers.conv2d(x, filters, 3, 1, 'same', activation=tf.nn.relu, name='conv1')
         for i in range(2):
-            filters *= 2   # 128, 256
+            filters *= 2  # 128, 256
             size = (size[0] // 2, size[1] // 2)
             # [-1, 14, 14, 128]   [-1, 7, 7, 258]
-            x = tf.layers.conv2d(x, filters, 3, 2, 'same',activation=tf.nn.relu, name='conv2_%d' % i)
+            x = tf.layers.conv2d(x, filters, 3, 2, 'same', activation=tf.nn.relu, name='conv2_%d' % i)
             # 14*14*256
             t = tf.layers.dense(l, size[0] * size[1] * filters, name='dense1_%d' % i)
             # [-1, 14, 14, 128]
@@ -128,19 +132,20 @@ class MySubTensors:
             x += t
 
         x = tf.layers.flatten(x)
-        x = tf.layers.dense(x, self.cfg.hidden_units)
+        x = tf.layers.dense(x, self.cfg.hidden_units, activation=tf.nn.relu, name='dense1')
         x = tf.nn.dropout(x, self.cfg.keep_prob)
-        x = tf.layers.dense(x, 1, name='dense')  # [-1, 1]
-        return tf.nn.sigmoid(x)
+        return tf.layers.dense(x, logits, name='dense2')
 
-    def gene(self, v, l):
+    def gene(self, x, l):
         '''
 
-        :param v: [-1, 4]
+        :param v: [-1, 28, 28, 1]
         :param l: [-1, 10]
         :return:
         '''
         filters = self.cfg.base_filters * 4
+        v = self.encode(x, l, self.cfg.vec_size)  # [-1, vec_size]
+
         size = 7
         v = tf.layers.dense(v, 7 * 7 * filters, activation=tf.nn.relu, name='dense')
         v = tf.reshape(v, [-1, 7, 7, filters])
@@ -148,28 +153,29 @@ class MySubTensors:
         for i in range(2):
             filters //= 2
             size *= 2
-            v = tf.layers.conv2d_transpose(v, filters, 3, 2, 'same',
-                                           activation=tf.nn.relu, name='deconv1_%d' % i)
+            v = tf.layers.conv2d_transpose(v, filters, 3, 2, 'same', activation=tf.nn.relu, name='deconv1_%d' % i)
+
             t = tf.layers.dense(l, size * size * filters, name='dense2_%d' % i)
             t = tf.reshape(t, [-1, size, size, filters])
             v += t
+
         # [-1,28, 28, filters]
         v = tf.layers.conv2d_transpose(v, 1, 3, 1, 'same', activation=tf.nn.relu, name='deconv2')
         return v  # [-1, 28, 28, 1]
 
 
 class MyAPP(myf.App):
-    def before_epoch(self, epoch):
-        self.config.random_seed()
 
     def after_batch(self, epoch, batch):
         print(epoch, '-----', batch)
 
     def test(self, ds_test):
-        vs = np.random.normal(size=[200, self.config.vec_size])
-        ls = [e % 10 for e in range(np.shape(vs)[0])]
+        cfg = self.config
+        ls = [e % 10 for e in range(cfg.batch_size)]
+        xs, _ = ds_text.next_batch(cfg.batch_size)
         ts = self.ts.sub_ts[-1]
-        imgs = self.session.run(ts.x2, {ts.v: vs, ts.lv: ls})*255  # [-1, 28, 28]
+        imgs = self.session.run(ts.x2, {ts.x: xs, ts.l: ls})*255  # [-1, 28, 28]
+
         imgs = np.reshape(imgs, [-1, 10, 28, 28])
         imgs = np.transpose(imgs, [0, 2, 1, 3])  # [-1, 28, 10, 28]
         imgs = np.reshape(imgs, [-1, 10, 28, 28])
@@ -198,6 +204,7 @@ class MyTensors(myf.Tensors):
 
     def get_loss_for_summary(self, loss):
         return tf.sqrt(loss) if loss == self.losses[-1] else loss
+
 
 def main():
     MyConfig().from_cmd()
